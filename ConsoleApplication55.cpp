d5cf74ba051c5e92b3535d998aa78d8abe313445
Zakaria-Hossam-code <zakariahossam261@gmail.com>
2025-11-17 23:30:12 +0200

    Add files via upload

---
+++ ConsoleApplication55.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int RondomNumInRange(int From, int To)
+{
+	int Num = rand() % (To - From + 1) + From;
+
+	return Num;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	cout << "Matrix 1 : " << endl;
+	int arr[3][3];
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			arr[i][j] = RondomNumInRange(1, 10);
+		}
+	}
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[i][j] << "   ";
+		}
+		cout << endl;
+	}
+
+	cout << "Middle Row : " << endl;
+	
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[1][j] << "   ";
+		}
+		cout << endl;
+
+	cout << "Middle Col : " << endl;
+	for (int i = 0; i < 3;i++)
+	{
+		cout << setw(3) << arr[i][1] << "   ";
+	}
+}
