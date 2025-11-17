5da6affa6145705070ffffcd1efcc4728cf5adfc
Zakaria-Hossam-code <zakariahossam261@gmail.com>
2025-11-17 22:23:42 +0200

    Add files via upload

---
+++ ConsoleApplication55.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int RondomNumInRange(int From, int To)
+{
+	int Num = rand() % (To - From + 1) + From;
+
+	return Num;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	cout << "Matrix 1 : " << endl;
+	int arr[3][3];
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			arr[i][j] = RondomNumInRange(1, 10);
+		}
+	}
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[i][j] << "   ";
+		}
+		cout << endl;
+	}
+
+	cout << "Matrix 2 : " << endl;
+	int arr2[3][3];
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			arr2[i][j] = RondomNumInRange(1, 10);
+		}
+	}
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr2[i][j] << "   ";
+		}
+		cout << endl;
+	}
+
+	cout << "Results : " << endl;
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[i][j] * arr2[i][j] << "   ";
+		}
+		cout << endl;
+	}
+}
