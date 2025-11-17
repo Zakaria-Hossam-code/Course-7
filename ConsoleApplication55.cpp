245ee59a276f4d58b0f7a7df5448c323b9880c73
Zakaria-Hossam-code <zakariahossam261@gmail.com>
2025-11-17 23:49:41 +0200

    Add files via upload

---
+++ ConsoleApplication55.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int RondomNumInRange(int From, int To)
+{
+	int Num = rand() % (To - From + 1) + From;
+
+	return Num;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	cout << "Matrix 1 : " << endl;
+	int arr[3][3];
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			arr[i][j] = RondomNumInRange(1, 10);
+		}
+	}
+	int counter = 0;
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[i][j] << "   ";
+			counter += arr[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << "Result : " << counter <<endl;
+
+
+	cout << "Matrix 2 : " << endl;
+	int arr2[3][3];
+
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			arr2[i][j] = RondomNumInRange(1, 10);
+		}
+	}
+	int counter2 = 0;
+	for (int i = 0; i < 3;i++)
+	{
+		for (int j = 0; j < 3;j++)
+		{
+			cout << setw(3) << arr[i][j] << "   ";
+			counter2 += arr2[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << "Result : " << counter2 << endl;
+
+
+
+	if (counter == counter2)
+	{
+		cout << "HA3 :-)";
+	}
+	else 
+		cout << "MESH HA3 :-(";
+}
