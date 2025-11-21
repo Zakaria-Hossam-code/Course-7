7694b1ee44ee58bb3358d91f6cbd8c8362a27a10
Zakaria-Hossam-code <zakariahossam261@gmail.com>
2025-11-21 15:06:15 +0200

    Create Problem 37.cpp

---
+++ Problem 37.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+string ReadString()
+{
+	string S;
+	cout << "Please Enter a String : ";
+	getline(cin, S);
+	return S;
+}
+
+vector <string> Split(string S, string D = " ")
+{
+	vector <string> vString;
+	int pos = 0;
+	string sWord;
+
+	while ((pos = S.find(D)) != std::string::npos)
+	{
+		sWord = S.substr(0, pos);
+
+		if (sWord != " ")
+		{
+			vString.push_back(sWord);
+		}
+		S.erase(0, pos + D.length());
+	}
+
+	if (S != "")
+	{
+		vString.push_back(S);
+	}
+
+	
+	return vString;
+}
+
+
+int main()
+{
+	vector <string> vString; 	
+	vString = Split(ReadString());
+
+	cout << "Tokens = " << vString.size() << endl;
+
+	for (string word : vString)
+	{
+		cout << word << endl;
+	}
+}
